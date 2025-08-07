13f66a0e47fcc51272ae7b435b888b6ec28199d7
IND024 <jtmhkl.7505997672@gmail.com>
2025-08-07 11:54:59 +0530

    Update geminiService.ts

--- geminiService.ts
+++ geminiService.ts
@@ -2,11 +2,7 @@
 import { GoogleGenAI, Type, GenerateContentResponse } from "@google/genai";
 import { BoardState, Player, Difficulty } from "../types";
 
-const API_KEY = process.env.API_KEY;
-
-if (!API_KEY) {
-  throw new Error("API_KEY environment variable not set");
-}
+const API_KEY = 'AIzaSyC4AMDc8z8UAYr84z1mci-OK0jyZKzgdbc'; 
 
 const ai = new GoogleGenAI({ apiKey: API_KEY });
 
